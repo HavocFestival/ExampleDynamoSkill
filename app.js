5cc8f4ffbf7c6bc1c7a0c88650adcce2f0a20b94
Wolfknightx <Wolfknightx@Gmail.com>
2018-10-13 14:19:43 -0500

    minor changes to the code structure

--- app.js
+++ app.js
@@ -7,101 +7,104 @@ const STOP_MESSAGE = 'Goodbye!';
 
 const skillBuilder = Alexa.SkillBuilders.standard();
 
-AWS.config.update({region:'us-east-1'});
+AWS.config.update({ region: 'us-east-1' });
 var dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-var dataParams = {
-  TableName: 'ExampleDynamoTable',
-  Key: { "ItemCode": "BUI" }
-};
-
 const GetNewFactHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'LaunchRequest'
-      || (request.type === 'IntentRequest'
-        && request.intent.name === 'DefineItemIntent');
-  },
-  async handle(handlerInput) {
-    console.log("Enter handler");
-    console.log(dataParams);
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'LaunchRequest' ||
+            (request.type === 'IntentRequest' &&
+                request.intent.name === 'DefineItemIntent');
+    },
+    async handle(handlerInput) {
+        const userInput = handlerInput.requestEnvelope.request.intent.slots.itemslot.value;
+        console.log(userInput);
+        var dataParams = {
+            TableName: 'ExampleDynamoTable',
+            Key: { "ItemCode": "BUI" }
+        };
 
-    dataParams.ItemCode = "DUI";
-    
-    var speechOutput = await dynamoDb.get(dataParams).promise();
 
-    console.log(speechOutput);
-    console.log(speechOutput.Item);
-    console.log(speechOutput.Item.ItemDescription);
+        console.log("Enter handler");
+        console.log(dataParams);
 
-    speechOutput = speechOutput.Item.ItemDescription;
+        dataParams.ItemCode = "DUI";
 
-    return handlerInput.responseBuilder
-      .speak(speechOutput)
-      .getResponse();
-  }
+        var speechOutput = await dynamoDb.get(dataParams).promise();
+
+        console.log(speechOutput);
+        console.log(speechOutput.Item);
+        console.log(speechOutput.Item.ItemDescription);
+
+        speechOutput = speechOutput.Item.ItemDescription;
+
+        return handlerInput.responseBuilder
+            .speak(speechOutput)
+            .getResponse();
+    }
 };
 
 const HelpHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(HELP_MESSAGE)
-      .reprompt(HELP_REPROMPT)
-      .getResponse();
-  }
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+            request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak(HELP_MESSAGE)
+            .reprompt(HELP_REPROMPT)
+            .getResponse();
+    }
 };
 
 const ExitHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && (request.intent.name === 'AMAZON.CancelIntent'
-        || request.intent.name === 'AMAZON.StopIntent');
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(STOP_MESSAGE)
-      .getResponse();
-  }
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+            (request.intent.name === 'AMAZON.CancelIntent' ||
+                request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        return handlerInput.responseBuilder
+            .speak(STOP_MESSAGE)
+            .getResponse();
+    }
 };
 
 const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
 
-    return handlerInput.responseBuilder.getResponse();
-  }
+        return handlerInput.responseBuilder.getResponse();
+    }
 };
 
 const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.message}`);
 
-    return handlerInput.responseBuilder
-      .speak('Sorry, an error occurred.')
-      .reprompt('Sorry, an error occurred.')
-      .getResponse();
-  }
+        return handlerInput.responseBuilder
+            .speak('Sorry, an error occurred.')
+            .reprompt('Sorry, an error occurred.')
+            .getResponse();
+    }
 };
 
 exports.handler = skillBuilder
-  .addRequestHandlers(
-    GetNewFactHandler,
-    HelpHandler,
-    ExitHandler,
-    SessionEndedRequestHandler
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
+    .addRequestHandlers(
+        GetNewFactHandler,
+        HelpHandler,
+        ExitHandler,
+        SessionEndedRequestHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
